4cc5af4dadb49f00dca21c512735e5275a6de43b
imedev2 <fatima@5w2h.biz>
2018-11-06 10:23:07 +0300

    First commit

---
+++ src/WebsiteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Fatima\Website;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
